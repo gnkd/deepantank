36706ab290973a5060b0b73949123cdfa0ef3780
gnkd <52245232+gnkd@users.noreply.github.com>
2021-01-19 15:24:43 +0530

    Initial commit

---
+++ simpan.php
@@ -0,0 +1,23 @@
+<?php
+
+require require 'firebase-php-master/src/firebaseLib.php';
+$param1 = $_GET["temp"];
+$param2 = $_GET["hum"];
+
+$url = 'https://Paste url firebase.firebaseio.com/'; 
+$token = 'Paste Tokoken Firebase'; 
+
+$DEFAULT_PATH = '/DTproduction';
+
+$_devicestatus= array(
+'suhu' => $param1,
+'kelembaban' => $param2,
+);
+
+ 
+
+$firebase = new \Firebase\FirebaseLib($url, $token);
+$firebase->update($DEFAULT_PATH, $_devicestatus);
+
+print("Update Berhasil");
+?>
